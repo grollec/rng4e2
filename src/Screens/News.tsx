44eb709d6987815ae8014b8f5afd714b1ee65afa
Renaud Bellec <renaud.bellec.3@gmail.com>
2024-05-10 15:33:36 +0200

    feat: paginated news

--- src/Screens/News.tsx
+++ src/Screens/News.tsx
@@ -1,20 +1,19 @@
-import {useQuery} from '@tanstack/react-query';
+import {keepPreviousData, useInfiniteQuery} from '@tanstack/react-query';
 import {isArray} from 'lodash';
-import React, {useCallback} from 'react';
+import React, {useMemo, useState} from 'react';
 import {
+  Button,
   SafeAreaView,
   StyleSheet,
   Text,
   View,
   VirtualizedList,
-  VirtualizedListProps,
 } from 'react-native';
 import {isRawNews, parseRawNews} from '../parsers/news';
 import {Article} from '../types/Article';
 
 const PER_PAGE = 20;
-const PAGE = 1;
-const OFFSET = 0;
+const INITIAL_PAGE = 0;
 const NEWS_QUERY_KEY = 'news';
 
 function parseNewsResponse(data: unknown): Article[] {
@@ -24,9 +23,11 @@ function parseNewsResponse(data: unknown): Article[] {
   return [];
 }
 
-async function fetchNews() {
+async function fetchNews({pageParam}: {pageParam: number}) {
   const res = await fetch(
-    `https://www.girondins4ever.com/wp-json/wp/v2/breves?per_page=${PER_PAGE}&page=${PAGE}&offset=${OFFSET}`,
+    `https://www.girondins4ever.com/wp-json/wp/v2/breves?per_page=${PER_PAGE}&offset=${
+      pageParam * PER_PAGE
+    }`,
     {},
   );
   const rawNews = await res.json();
@@ -47,28 +48,57 @@ function getItem(data: Article[], index: number) {
   return data[index];
 }
 
-function getItemCount(data: Article[]) {
-  return data.length;
+function getItemCount(data?: Article[]) {
+  return data?.length || 0;
 }
 
 export const News = () => {
-  const {data, isLoading, isError} = useQuery({
+  const [showLoadMore, setShowLoadMore] = useState(false);
+  const {
+    data,
+    refetch,
+    isRefetching,
+    isPlaceholderData,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
     queryKey: [NEWS_QUERY_KEY],
     queryFn: fetchNews,
-    placeholderData: [],
+    initialPageParam: INITIAL_PAGE,
+    getNextPageParam: (lastPage, _, lastPageParam) => {
+      if (lastPage.length === 0) {
+        return undefined;
+      }
+      return lastPageParam + 1;
+    },
+    placeholderData: keepPreviousData,
   });
 
   console.log(data);
+
+  const news = useMemo(() => {
+    return data?.pages.flat() || [];
+  }, [data?.pages]);
 
   return (
     <SafeAreaView style={styles.container}>
       <VirtualizedList<Article>
         initialNumToRender={PER_PAGE}
         renderItem={item => <Item article={item.item} />}
-        data={data}
+        data={news}
         getItem={getItem}
         getItemCount={getItemCount}
+        onRefresh={refetch}
+        refreshing={isRefetching}
+        onEndReached={() => setShowLoadMore(true)}
       />
+      {showLoadMore && !isPlaceholderData ? (
+        <Button
+          disabled={isFetchingNextPage}
+          onPress={() => fetchNextPage()}
+          title="Load more"
+        />
+      ) : null}
     </SafeAreaView>
   );
 };
